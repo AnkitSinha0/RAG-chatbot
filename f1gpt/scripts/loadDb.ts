ffca3ddb49d016dc2d05b2d53af26fd1228c329d
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2025-10-02 22:14:13 +0530

    new addons

--- f1gpt/scripts/loadDb.ts
+++ f1gpt/scripts/loadDb.ts
@@ -3,7 +3,9 @@ import {PuppeteerWebBaseLoader}  from "langchain/document_loaders/web/puppeteer"
 import OpenAI from "openai"
 import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
 import "dotenv/config"
+import { text } from "stream/consumers"
 
+type SimilarityMetric = "dot_product"| "cosine" | "euclidean"
 
 const {ASTRA_DB_NAMESPACE,
     ASTRA_DB_COLLECTION, 
@@ -16,5 +18,60 @@ const openai = new OpenAI({apiKey : OPENAI_API_KEY})
 
 const f1Data = [
     'https://en.wikipedia.org/wiki/Formula_One',
-    
+    'https://en.wikipedia.org/wiki/2024_Formula_One_World_Championship',
+    'https://en.wikipedia.org/wiki/2025_Formula_One_World_Championship',
+    'https://www.skysports.com/f1/news/12040/12971472/f1-2024-calendar-dates-line-ups-and-results-formula-1-gp-schedule-on-sky-sports-and-race-highlights',
+    'https://www.sportskeeda.com/f1/f1-schedule',
+    'https://www.motorsport.com/f1/standings/2024/',
+    'https://www.espn.com/racing/results/_/series/f1/year/2024',
+    'https://en.wikipedia.org/wiki/Lewis_Hamilton',
+    'https://en.wikipedia.org/wiki/Max_Verstappen',
 ]
+
+const client  = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN)
+
+const db = client.db(ASTRA_DB_API_ENDPOINT,{namespace : ASTRA_DB_NAMESPACE})
+
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize:512,
+    chunkOverlap:100,
+})
+
+const createCollection = async (similarityMetric : SimilarityMetric = "dot_product") => {
+  const res =   await db.createCollection(ASTRA_DB_COLLECTION,
+    {
+        vector : {
+            dimension :  1536,
+            metric : similarityMetric,
+        }
+    })
+    console.log(res)
+}
+
+const loadSampleData = async () => {
+    const collection = await db.collection(ASTRA_DB_COLLECTION)
+    for await  (const url of f1Data){
+        const content =  await scrapePage(url)
+        const chunks  = await splitter.splitText(content)
+        for await (const chunk of chunks){
+            const embedding = await openai.embeddings.create({
+                model : "text-embedding-3-small",
+                input : chunk,
+                encoding_format : "float"
+            })
+
+            const vector = embedding.data[0].embedding
+
+            const res = await.collection.insertOne({
+                $vector : vector,
+                text:chunk,
+            })
+            console.log(res)
+        }
+
+    }
+}
+
+const scrapePage = async(url : string) => {
+    
+}
