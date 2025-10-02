f752efa254b76443d467462ad623cea53860a36d
Ankit Sinha <119648425+AnkitSinha0@users.noreply.github.com>
2025-10-02 23:41:03 +0530

    new

--- f1gpt/scripts/loadDb.ts
+++ f1gpt/scripts/loadDb.ts
@@ -73,5 +73,18 @@ const loadSampleData = async () => {
 }
 
 const scrapePage = async(url : string) => {
-    
+    const loader = new PuppeteerWebBaseLoader(url,{
+        launchOptions: {
+            headless : true
+        },
+        gotoOptions:{
+            waitUntil : "domcontentloaded"
+        },
+        evaluate : async ( page , browser) => {
+            const result = await page.evaluate(() => document.body.innerHTML)
+            await browser.close()
+            return result
+        }
+    })
+    return (await loader.scrape())?.replace(/<[^>]*>?/gm,'')
 }
